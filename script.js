8ddd72c275ae8668ba8ebac75a63ee12653d42ad
Doertie <julian.konken@gmx.net>
2026-02-02 21:47:17 +0100

    implement shot animation

--- script.js
+++ script.js
@@ -10,9 +10,19 @@ function renderQuestion() {
 
 function submitAnswer() {
   let answer = document.getElementById('answer');
-  
+  let shipShot = document.getElementById('spaceShipShot')
+
   if (answer.value == vocabulary_db[rendomNum].answer) {
+    shipShot.classList.add('spaceship_shot');
+    shipShot.style.animation = "shipShot 2s"
+
+    setTimeout(() => {
+      shipShot.classList.remove('spaceship_shot');
+      shipShot.style.animation = ""
+      renderQuestion()
+    }, 2000);
     console.log('that was right');
+
   } else {
     console.log('that was not correct');
   }
