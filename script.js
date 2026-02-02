262acba595467798590cb8fcb66da9d4b316819c
Doertie <julian.konken@gmx.net>
2026-02-02 20:08:30 +0100

    implement render and submit function

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+let rendomNum = 0;
 
 function renderQuestion() {
   let refQuestion = document.getElementById('question');
+  rendomNum = Math.floor(Math.random() * vocabulary_db.length);
+
+  let question = vocabulary_db[rendomNum].question;
+  refQuestion.innerHTML = question;
+}
+
+function submitAnswer() {
+  let answer = document.getElementById('answer');
   
-  refQuestion.innerHTML = vocabulary_db[0].question;
+  if (answer.value == vocabulary_db[rendomNum].answer) {
+    console.log('that was right');
+  } else {
+    console.log('that was not correct');
+  }
 }
