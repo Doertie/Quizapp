47052a6932c87c87b3ee96ae6083e6366da28567
Doertie <julian.konken@gmx.net>
2026-02-02 18:20:12 +0100

    implement vocabulary_db and style buttons

---
+++ js/vocabulary_db.js
@@ -0,0 +1,22 @@
+const vocabulary_db = [
+  {
+    "question": "wie",
+    "answer": "how"
+  },
+  {
+    "question": "wer",
+    "answer": "who"
+  },
+  {
+    "question": "wo",
+    "answer": "where"
+  },
+  {
+    "question": "durch",
+    "answer": "throught"
+  },
+  {
+    "question": "warum",
+    "answer": "why"
+  },
+];
---
+++ script.js
@@ -0,0 +1,6 @@
+
+function renderQuestion() {
+  let refQuestion = document.getElementById('question');
+  
+  refQuestion.innerHTML = vocabulary_db[0].question;
+}
